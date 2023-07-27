e7a1ae75fa6d578dd54bf501800226378cbc9617
Nirdosh <bimallamixane2@gmail.com>
2023-07-27 15:23:18 +0545

    404 page maintained

--- frontend/src/pages/Global/globalError.jsx
+++ frontend/src/pages/Global/globalError.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 export function GlobalError() {
   return (
-    <div>
-      <h1 className="text-9xl text-dark-200">GlobalError</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="text-6xl font-bold text-green-600">404</div>
+      <div className="text-2xl font-medium text-gray-600">Page Not Found</div>
+      <p className="text-gray-500 mt-2">
+        Oops! The page you are looking for does not exist.
+      </p>
+      <button
+        className="mt-4 px-6 py-2 text-white bg-green-600 rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500"
+        onClick={() => window.history.back()}
+      >
+        Go Back
+      </button>
     </div>
   );
 }
